ea9e6cada3f92f6afa309169250e43dece3bdb1b
Luis Blanco <luisblanco1337@gmail.com>
2026-08-01 16:36:56 +0400

    Fix installed deps-opengl rpath

--- install.js
+++ install.js
@@ -1,6 +1,6 @@
 import { install } from '@node-3d/addon-tools';
 
 const prefix = 'https://github.com/node-3d/webgl/releases/download';
-const tag = '6.0.0';
+const tag = '6.0.1';
 
 await install(`${prefix}/${tag}`);
